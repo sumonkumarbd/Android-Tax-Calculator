31c55767a1727549c58e7a0c8f02a30b166f451e
sumonkmr <sumonkmr.dev@gmail.com>
2022-01-02 14:48:24 +0600

    salary_tax.xml building design!!

--- app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
+++ app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
@@ -3,8 +3,13 @@ package com.sumonkmr.taxcalculator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SalaryTax extends FreedomFighterTax {
+
+    LinearLayout mainCard_salary;
+    TextView appName_salary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +19,22 @@ public class SalaryTax extends FreedomFighterTax {
         //        Object of Handler
         Handler handler = new Handler();
 
+//        HOOKS
+        mainCard_salary = findViewById(R.id.mainCard_salary);
+        appName_salary = findViewById(R.id.appName_salary);
+
+
         //        for TextMorque
         Runnable taxt = () ->text_morq.setEllipsize(TextUtils.TruncateAt.MARQUEE);
         text_morq.setSelected(true);
         handler.postDelayed(taxt,2000);
 
+
+
+//        Caling Animations
+
+        appName_salary.setAnimation(zoom_in);
+        mainCard_salary.setAnimation(fade_in);
 
 
 
