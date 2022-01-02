3641e34ec870044f9d5ea9e85d9520182614c076
sumonkmr <sumonkmr.dev@gmail.com>
2022-01-02 13:28:07 +0600

    SalaryTax class & salary_tax.xml added && textMoeque is fixed!!

--- app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
+++ app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
@@ -4,19 +4,30 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 
-public class SalaryTax extends FreedomFighterTax{
+public class SalaryTax extends FreedomFighterTax {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.salaey_tax);
+        setContentView(R.layout.salary_tax);
 
-//        objects
-        FreedomFighterTax freedomFighterTax = new FreedomFighterTax();
+        //        Object of Handler
         Handler handler = new Handler();
-//        for TextMorque
+
+        //        for TextMorque
         Runnable taxt = () ->text_morq.setEllipsize(TextUtils.TruncateAt.MARQUEE);
         text_morq.setSelected(true);
         handler.postDelayed(taxt,2000);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+    }//onCreate
+}// class SalaryTax
