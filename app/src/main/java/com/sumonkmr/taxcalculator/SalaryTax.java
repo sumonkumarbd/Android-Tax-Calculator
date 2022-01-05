28a94f51eba6db2fb83d5146ec7c325c03867be3
sumonkmr <sumonkmr.dev@gmail.com>
2022-01-05 15:45:26 +0600

    add EditText field in tax salary_tax.xml's all id id Hooked in SalaryTax class for implements!!

--- app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
+++ app/src/main/java/com/sumonkmr/taxcalculator/SalaryTax.java
@@ -3,6 +3,7 @@ package com.sumonkmr.taxcalculator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class SalaryTax extends FreedomFighterTax {
 
     LinearLayout mainCard_salary;
     TextView appName_salary;
+    EditText basic_salary,bonus,past_salary,house_rent,medical_allowance,surgery_cost,travel_cost,festival_bonus,servant_allowance,holiday_allowance,honorary_gift,over_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,18 @@ public class SalaryTax extends FreedomFighterTax {
 //        HOOKS
         mainCard_salary = findViewById(R.id.mainCard_salary);
         appName_salary = findViewById(R.id.appName_salary);
+        basic_salary = findViewById(R.id.basic_salary);
+        bonus = findViewById(R.id.bonus);
+        past_salary = findViewById(R.id.past_salary);
+        house_rent = findViewById(R.id.house_rent);
+        medical_allowance = findViewById(R.id.medical_allowance);
+        surgery_cost = findViewById(R.id.surgery_cost);
+        travel_cost = findViewById(R.id.travel_cost);
+        festival_bonus = findViewById(R.id.festival_bonus);
+        servant_allowance = findViewById(R.id.servant_allowance);
+        holiday_allowance = findViewById(R.id.holiday_allowance);
+        honorary_gift = findViewById(R.id.honorary_gift);
+        over_time = findViewById(R.id.over_time);
 
 
         //        for TextMorque
